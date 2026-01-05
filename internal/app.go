278072dbc5f0da8a3342aedfcfd2a9e2286a27f6
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-01-05 07:04:53 +0700

    [app] wire up modules

--- internal/app.go
+++ internal/app.go
@@ -4,8 +4,15 @@ import (
 	"context"
 
 	"github.com/apiarycd/apiarycd/internal/config"
-	"github.com/apiarycd/apiarycd/internal/example"
+	"github.com/apiarycd/apiarycd/internal/deployments"
 	"github.com/apiarycd/apiarycd/internal/server"
+	"github.com/apiarycd/apiarycd/internal/stacks"
+	"github.com/apiarycd/apiarycd/internal/swarm"
+	"github.com/apiarycd/apiarycd/pkg/badgerfx"
+	"github.com/apiarycd/apiarycd/pkg/dockerfx"
+	"github.com/go-core-fx/fiberfx"
+	"github.com/go-core-fx/fiberfx/health"
+	"github.com/go-core-fx/healthfx"
 	"github.com/go-core-fx/logger"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
@@ -16,28 +23,30 @@ func Run() {
 		// CORE MODULES
 		logger.Module(),
 		logger.WithFxDefaultLogger(),
-		// sqlfx.Module(),
-		// goosefx.Module(),
-		// bunfx.Module(),
-		// fiberfx.Module(),
+		badgerfx.Module(),
+		dockerfx.Module(),
+		healthfx.Module(),
+		fiberfx.Module(),
 		//
 		// APP MODULES
 		config.Module(),
-		// db.Module(),
 		server.Module(),
-		// bot.Module(),
+		swarm.Module(),
 		//
 		// BUSINESS MODULES
-		example.Module(),
+		fx.Provide(func() health.Version { return health.Version{Version: "0.0.1", ReleaseID: 1} }),
+		stacks.Module(),
+		deployments.Module(),
 		//
+		// LIFECYCLE MANAGEMENT
 		fx.Invoke(func(lc fx.Lifecycle, logger *zap.Logger) {
 			lc.Append(fx.Hook{
 				OnStart: func(_ context.Context) error {
-					logger.Info("app started")
+					logger.Info("🚀 ApiaryCD application starting up")
 					return nil
 				},
 				OnStop: func(_ context.Context) error {
-					logger.Info("app stopped")
+					logger.Info("🛑 ApiaryCD application shutting down gracefully")
 					return nil
 				},
 			})
