3e199e70ded275bd1b53bf4dcfd8e53bc866d423
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-01-08 15:41:37 +0700

    [dev] install swag, reuse request responses and limit swag generation

--- swagger.mk
+++ swagger.mk
@@ -5,7 +5,7 @@ swagger: ## Generate Swagger documentation
 		echo "Error: Failed to format API docs"; \
 		exit 1; \
 	fi
-	@if ! swag init --parseDependency -g ./main.go -o ./internal/server/docs; then \
+	@if ! swag init --parseDependency --outputTypes go -g ./main.go -o ./internal/server/docs; then \
 		echo "Error: Failed to generate API docs"; \
 		exit 1; \
 	fi
