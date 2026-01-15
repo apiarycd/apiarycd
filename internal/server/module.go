ed29c5dd7e49cbf10976d8a7405defff4306a225
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-01-15 07:50:33 +0700

    [server] update validatorfx

--- internal/server/module.go
+++ internal/server/module.go
@@ -1,9 +1,6 @@
 package server
 
 import (
-	"reflect"
-	"strings"
-
 	"github.com/apiarycd/apiarycd/internal/server/docs"
 	"github.com/apiarycd/apiarycd/internal/server/handlers/stacks"
 	"github.com/apiarycd/apiarycd/pkg/openapifx"
@@ -12,7 +9,6 @@ import (
 	"github.com/go-core-fx/fiberfx/health"
 	"github.com/go-core-fx/fiberfx/validation"
 	"github.com/go-core-fx/logger"
-	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
@@ -31,17 +27,6 @@ func Module() fx.Option {
 		}),
 		fx.Supply(docs.SwaggerInfo),
 
-		fx.Decorate(func(v *validator.Validate) *validator.Validate {
-			v.RegisterTagNameFunc(func(fld reflect.StructField) string {
-				//nolint:mnd //fixed length
-				name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
-				if name == "-" {
-					return ""
-				}
-				return name
-			})
-			return v
-		}),
 		fx.Provide(
 			fx.Annotate(health.NewHandler, fx.ResultTags(`name:"health-handler"`)), fx.Private,
 			fx.Annotate(stacks.NewHandler, fx.ResultTags(`group:"handlers"`)), fx.Private,
@@ -55,7 +40,7 @@ func Module() fx.Option {
 
 					// Version 1 API group
 					v1 := app.Group("/api/v1")
-					openapiHandler.Register(v1.Group("docs"))
+					openapiHandler.Register(v1.Group("/docs"))
 
 					v1.Use(validation.Middleware)
 
